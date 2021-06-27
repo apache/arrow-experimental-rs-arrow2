f1e4a8535ce05fcdc4ea22e41afd129a0ac6b4bd
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-06-27 05:08:47 +0000

    Added licenses.

--- dev/release/check-rat-report.py
+++ dev/release/check-rat-report.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-##############################################################################
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -16,15 +14,14 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-##############################################################################
+
 import fnmatch
 import re
 import sys
 import xml.etree.ElementTree as ET
 
 if len(sys.argv) != 3:
-    sys.stderr.write("Usage: %s exclude_globs.lst rat_report.xml\n" %
-                     sys.argv[0])
+    sys.stderr.write("Usage: %s exclude_globs.lst rat_report.xml\n" % sys.argv[0])
     sys.exit(1)
 
 exclude_globs_filename = sys.argv[1]
@@ -34,26 +31,28 @@ globs = [line.strip() for line in open(exclude_globs_filename, "r")]
 
 tree = ET.parse(xml_filename)
 root = tree.getroot()
-resources = root.findall('resource')
+resources = root.findall("resource")
 
 all_ok = True
 for r in resources:
-    approvals = r.findall('license-approval')
-    if not approvals or approvals[0].attrib['name'] == 'true':
+    approvals = r.findall("license-approval")
+    if not approvals or approvals[0].attrib["name"] == "true":
         continue
-    clean_name = re.sub('^[^/]+/', '', r.attrib['name'])
+    clean_name = re.sub("^[^/]+/", "", r.attrib["name"])
     excluded = False
     for g in globs:
         if fnmatch.fnmatch(clean_name, g):
             excluded = True
             break
     if not excluded:
-        sys.stdout.write("NOT APPROVED: %s (%s): %s\n" % (
-            clean_name, r.attrib['name'], approvals[0].attrib['name']))
+        sys.stdout.write(
+            "NOT APPROVED: %s (%s): %s\n"
+            % (clean_name, r.attrib["name"], approvals[0].attrib["name"])
+        )
         all_ok = False
 
 if not all_ok:
     sys.exit(1)
 
-print('OK')
+print("OK")
 sys.exit(0)
--- src/types/simd/packed.rs
+++ src/types/simd/packed.rs
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 pub use packed_simd::{
     f32x16, f64x8, i16x32, i32x16, i64x8, i8x64, m16x32, m32x16, m64x8, m8x64, u16x32, u32x16,
     u64x8, u8x64,
